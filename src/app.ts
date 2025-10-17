16e808c37709632734cbe854bffda0a902a39404
myrn <okanbatuk@protonmail.com>
2025-10-17 13:43:47 +0300

    feat: add new route to run when the route not found

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import "reflect-metadata";
 import helmet from "helmet";
 import cors from "cors";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import router from "./routes.js";
 import { config, logger } from "./config/index.js";
 import { errorHandler, sendResponse } from "./utils/index.js";
+import { ApiError } from "./errors/api.error.js";
 
 export async function buildApp() {
   const ENV = process.env.NODE_ENV || config.env;
@@ -27,6 +28,13 @@ export async function buildApp() {
     return sendResponse(res, 200, undefined, "Ads API working on Server!");
   });
   app.use("/api", router);
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    try {
+      throw new ApiError("Route not found.", 404);
+    } catch (error) {
+      next(error);
+    }
+  });
   app.use(errorHandler);
 
   return app;
